413b97766498dc7e0d3851c5a382375aacd27a9e
Bohdan Fedorov <bogdanfedorov@gmail.com>
2024-08-11 00:23:44 +0300

    add export from components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { LinksList } from "./LinksList";
+import { Navbar } from "./Navbar";
+import { ThemeSwitch } from "./ThemeSwich";
+
+export * from "./icons";
+export { ThemeSwitch, Navbar, LinksList };
