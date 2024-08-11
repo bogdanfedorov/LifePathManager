456868714c0b50404c346819602ea4b3c699ad01
Bohdan Fedorov <bogdanfedorov@gmail.com>
2024-08-11 19:28:38 +0300

    update vacancy endpoints and actions for new "entity" flow

--- src/app/vacancy/create/actions.ts
+++ src/app/vacancy/create/actions.ts
@@ -1,16 +1,5 @@
 "use server";
-import { VacancyModel, VacancyRepository } from "@/models/VacancyModel";
+import { actionCreate } from "@/hooks/serverActions";
+import { Models } from "@/types";
 
-export const createVacancy = async (formData: FormData) => {
-  const vacancy = new VacancyModel({
-    companyName: String(formData.get("companyName")),
-    companyLink: String(formData.get("companyLink")),
-    salary: Number(formData.get("salary")),
-    status: String(formData.get("status")),
-    comment: String(formData.get("comment")),
-  });
-
-  const savedVacancy = await VacancyRepository.create(vacancy);
-
-  return savedVacancy.toPlainObject();
-};
+export const createVacancy = actionCreate(Models.vacancy);
--- src/app/vacancy/table/actions.ts
+++ src/app/vacancy/table/actions.ts
@@ -1,17 +1,6 @@
 "use server";
+import { actionDelete, actionGetPaginationList } from "@/hooks/serverActions";
+import { Models } from "@/types";
 
-import { IPagination } from "@/models/core/types";
-import { VacancyModelDto, VacancyRepository } from "@/models/VacancyModel";
-
-export const getVacancy = async (): Promise<IPagination<VacancyModelDto>> => {
-  const vacancyArray = await VacancyRepository.pagination({});
-
-  const data = vacancyArray.data.map((vacancy) => vacancy.toPlainObject());
-
-  await new Promise((r) => setTimeout(r, 2000));
-
-  return {
-    data,
-    metadata: vacancyArray.metadata,
-  };
-};
+export const getVacancy = actionGetPaginationList(Models.vacancy);
+export const deleteVacancy = actionDelete(Models.vacancy);
